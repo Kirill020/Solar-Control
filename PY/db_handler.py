404d6777336ed5a6cacbc7a48a64f63fe60fcffb
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-03-10 11:30:19 +0100

    Create db_handler.py

---
+++ PY/db_handler.py
@@ -0,0 +1,21 @@
+import sqlite3 as sql
+
+class sqliteDB:
+
+    @staticmethod
+    def add_user(P_name, P_email_adress):
+        conn = sql.connect("Solar_panel.db")
+        cursor = conn.cursor()
+        cursor.execute("INSER INTO People (P_name, P_email_adress) VALUES (?, ?)", (P_name, P_email_adress))
+        conn.commit()
+        conn.close()
+    
+    @staticmethod
+    def add_panel_group(Panels_amount, Panels_adress, Capasity):
+        conn = sql.connect("Solar_panel.db")
+        cursor = conn.cursor()
+        cursor.execute("INSER INTO Panels (Panels_amount, Panels_adress, Capasity) VALUES (?, ?, ?)", (Panels_amount, Panels_adress, Capasity))
+        conn.commit()
+        conn.close()
+    
+    
