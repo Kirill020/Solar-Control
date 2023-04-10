466e29af60ddc8d1a8e21233ca22efd474031e2f
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-04-10 22:44:23 +0200

    Update db_handler.py

--- PY/db_handler.py
+++ PY/db_handler.py
@@ -1,107 +1,155 @@
 import sqlite3 as sql
 import hashlib
+from datetime import datetime
 
 class SqliteDB:
 
+    # todo: sql-query to get an Id from Weather and Panels
 
     #authentication user
     @staticmethod
-    def authenticate_user(username: str, password: str) -> bool:
+    def authenticate_user(username: str, password: str) -> tuple[bool, int]:
         conn = sql.connect('Solar_panel.db')
         cursor = conn.cursor()
-        cursor.execute("SELECT P_pass FROM Users WHERE P_name = ?", (username,))
+        cursor.execute("SELECT P_pass, Person_id FROM Users WHERE P_name = ?", (username,))
         result = cursor.fetchone()
-
+        
         if result is None:
             conn.close()
-            return False
+            return False, None
 
         password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
         if password_hash == result[0]:
             conn.close()
-            return True
+            return True, result[1]
 
         conn.close()
-        return False
+        return False, None
 
 
-    #add user
+    #get wetaher_id
     @staticmethod
-    def add_user(P_name: str, P_email_adress: str, P_adress: str, Password: str):
+    def get_weather_id(id_panel_group: int) -> int:
         conn = sql.connect("Solar_panel.db")
         cursor = conn.cursor()
-        P_pass  = hashlib.sha256(Password.encode('utf-8')).hexdigest()
-        cursor.execute("INSER INTO Users (P_name, P_email_adress, P_adress, P_pass) VALUES (?, ?, ?, ?)", (P_name, P_email_adress, P_adress, P_pass))
+        cursor.execute("SELECT Weather_id FROM Weather WHERE Id_PanelGroup = ?", (id_panel_group,))
+        conn.commit()
+        
+        result = cursor.fetchone()
+        if result is None:
+            conn.close()
+            return None
+        else:
+            conn.close()
+            return result[0]
+        
+    
+    @staticmethod
+    def get_panel_group_id(person_id: int) -> int:
+        conn = sql.connect("Solar_panel.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT Id_PanelGroup FROM Panels WHERE Person_id = ?", (person_id,))
+        
+        result = cursor.fetchone()
+        if result is None:
+            conn.close()
+            return None
+        else:
+            conn.close()
+            return result[0]
+
+    
+    #add user
+    @staticmethod
+    def add_user(p_name: str, p_email_adress: str, p_adress: str, password: str):
+        conn = sql.connect("Solar_panel.db")
+        cursor = conn.cursor()
+        p_pass  = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        cursor.execute("INSERT INTO Users (P_name, P_email_adress, P_adress, P_pass) VALUES (?, ?, ?, ?)", (p_name, p_email_adress, p_adress, p_pass))
         conn.commit()
         conn.close()
     
     #add panel group
     @staticmethod
-    def add_panels_group(person_id: int, panels_amount: int, panels_adress: str, performance: float, m_performance: float, w_performance: float, d_performance: float):
+    def add_panels_group(person_id: int, panels_amount: int, panels_adress: str, performance: float, voltage: float, power: float):
         conn = sql.connect('Solar_panel.db')
         cursor = conn.cursor()
-        query = "INSERT INTO Panels (Person_id, Panels_amount, Panels_adress, Performance, M_performance, W_performance, D_performance) VALUES (?, ?, ?, ?, ?, ?, ?)"
-        values = (person_id, panels_amount, panels_adress, performance, m_performance, w_performance, d_performance)
+        date = datetime.now()
+        query = "INSERT INTO Panels (Person_id, Panels_amount, Panels_adress, Performance, Voltage, Power, Date) VALUES (?, ?, ?, ?, ?, ?, ?)"
+        values = (person_id, panels_amount, panels_adress, performance, voltage, power, date)
         cursor.execute(query, values)
         conn.commit()
         conn.close()
 
     #add weather data
     @staticmethod
-    def add_weather_data(id_panels_group: int, weather_type: str, date: str, temperature: float, wind_speed: float):
+    def add_weather_data(id_panels_group: int, weather_type: str, temperature: float, wind_speed: float):
         conn = sql.connect('Solar_panel.db')
         cursor = conn.cursor()
+        date = datetime.now()
         query = "INSERT INTO Weather (Id_PanelsGroup, Weather_type, Date, Temperature, Wind_speed) VALUES (?, ?, ?, ?, ?)"
         values = (id_panels_group, weather_type, date, temperature, wind_speed)
         cursor.execute(query, values)
         conn.commit()
         conn.close()
 
+
     #update user info
     @staticmethod
-    def update_user_data(Person_id: int, new_name: str, new_email_adress: str, new_adress:str, new_pass: str):
+    def update_user_data(person_id: int, new_name: str, new_email_adress: str, new_adress: str, new_pass: str) -> bool:
         conn = sql.connect('Solar_panel.db')
         cursor = conn.cursor()
-        if(new_name != None):
-            query = "UPDATE Users SET P_name = {new_name} WHERE Person_id = {Person_id}"
-            cursor.execute(query)
-            conn.commit()
-            conn.close()
 
-        elif(new_email_adress != None):
-            query = "UPDATE Users SET P_email_adress = {new_email_adress} WHERE Person_id = {Person_id}"
-            cursor.execute(query)
+        if new_name is not None:
+            query = "UPDATE Users SET P_name = ? WHERE Person_id = ?"
+            cursor.execute(query, (new_name, person_id))
             conn.commit()
-            conn.close()
+
+        if new_email_adress is not None:
+            query = "UPDATE Users SET P_email_adress = ? WHERE Person_id = ?"
+            cursor.execute(query, (new_email_adress, person_id))
+            conn.commit()
+
+        if new_adress is not None:
+            query = "UPDATE Users SET P_adress = ? WHERE Person_id = ?"
+            cursor.execute(query, (new_adress, person_id))
+            conn.commit()
+
+        if new_pass is not None:
+            password_hash = hashlib.sha256(new_pass.encode('utf-8')).hexdigest()
+            query = "UPDATE Users SET P_pass = ? WHERE Person_id = ?"
+            cursor.execute(query, (password_hash, person_id))
+            conn.commit()
+
+        cursor.execute("SELECT COUNT(*) FROM Users WHERE Person_id = ? LIMIT 1", (person_id,))
+        result = cursor.fetchone()
+        conn.close()
+
+        return result[0] == 1
+
         
-        elif(new_adress != None):
-            query = "UPDATE Users SET P_adress = {new_adress} WHERE Person_id = {Person_id}"
-            cursor.execute(query)
-            conn.commit()
-            conn.close()
-
-        elif(new_pass != None):
-            P_pass  = hashlib.sha256(new_pass.encode('utf-8')).hexdigest()
-            query = "UPDATE Users SET P_pass = {P_pass} WHERE Person_id = {Person_id}"
-            cursor.execute(query)
-            conn.commit()
-            conn.close()
-        
-
 
     #update panels data
-    #todo:      change DB(Panels) and set power, voltage and power
-    #           and change next method
-    
     @staticmethod
-    def update_penels_group(Panels_id: int):
+    def update_penels_group(panels_id: int, person_id: int, panels_amount: int, panels_adress: str, performance: float, voltage: float, power: float):
         conn = sql.connect('Solar_panel.db')
         cursor = conn.cursor()
-        query = "UPDATE Panels SET Performance = {P_pass} WHERE Id_PanelGroup = {Panels_id}"
-        #cursor.execute(query)
+        date = datetime.now()
+        query = "INSERT INTO Panels (Id_PanelGroup, Person_id, Panels_amount, Panels_adress, Performance, Voltage, Power, Date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        values = (panels_id, person_id, panels_amount, panels_adress, performance, voltage, power, date)
+        cursor.execute(query, values)
         conn.commit()
         conn.close()
 
-    #@staticmethod
-    #def upd_user()
+    #update weather data
+    @staticmethod
+    def upddate_weather(weather_id: int, id_panels_group: int, weather_type: str, temperature: float, wind_speed: float):
+        conn = sql.connect('Solar_panel.db')
+        cursor = conn.cursor()
+        date = datetime.now()
+        query = "INSERT INTO Weather (Weather_id, Id_PanelsGroup, Weather_type, Date, Temperature, Wind_speed) VALUES (?, ?, ?, ?, ?, ?)"
+        values = (weather_id, id_panels_group, weather_type, date, temperature, wind_speed)
+        cursor.execute(query, values)
+        conn.commit()
+        conn.close()
