55279f8a7e77a77f61c9251c91052886cf8be0d3
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-02 19:27:29 +0200

    Update api.py

--- PY/api.py
+++ PY/api.py
@@ -31,5 +31,5 @@ async def weather(id: int, data: dict):
 if __name__ == "__main__":
     import uvicorn
     
-    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
     
