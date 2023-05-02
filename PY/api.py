b711c04deeb99560757750228867259ab7206a85
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-02 17:20:45 +0200

    Update api.py

--- PY/api.py
+++ PY/api.py
@@ -6,7 +6,7 @@ from db_handler import SqliteDB
 # import models
 from models import AuthModel
 
-app = FastAPI(title="Solar Panel API", version="0.1.0", description="API for Solar Panel project")
+app = FastAPI(title="SolarControl API", version="0.1.0", description="API for SolarControl project")
 
 @app.get("/", tags=["Example route"])
 async def index():
@@ -16,7 +16,8 @@ async def index():
 async def auth(auth_data: AuthModel):
     password = auth_data.password
     username = auth_data.username
-    ...
+    print(username)
+    print(password)
 
 @app.get("/weather/{id}", tags=["Example route"])
 async def weather(id: int):
@@ -30,4 +31,5 @@ async def weather(id: int, data: dict):
 if __name__ == "__main__":
     import uvicorn
     
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+    
