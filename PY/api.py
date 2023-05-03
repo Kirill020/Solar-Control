4018153c2e1762c5c34675d86ce3a47e614e19d6
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-03 22:33:01 +0200

    Update api.py

--- PY/api.py
+++ PY/api.py
@@ -4,28 +4,35 @@ from fastapi import FastAPI
 from db_handler import SqliteDB
 
 # import models
-from models import AuthModel
+from models import CheckModel, GroupDataModel
 
 app = FastAPI(title="SolarControl API", version="0.1.0", description="API for SolarControl project")
 
-@app.get("/", tags=["Example route"])
+@app.get("/")
 async def index():
     return {"message": "Hello World"}
 
-@app.post("/auth", tags=["Example route"])
-async def auth(auth_data: AuthModel):
+@app.post("/check")
+async def auth(auth_data: CheckModel):
     password = auth_data.password
     username = auth_data.username
     print(username)
     print(password)
 
-@app.get("/weather/{id}", tags=["Example route"])
-async def weather(id: int):
-    return {"message": "weather for {id}"}
+@app.post("/group_data")
+async def add_group_data(group_data: GroupDataModel):
+    id = group_data.id
+    performance = group_data.performance
+    voltage = group_data.voltage
+    power = group_data.power
+    Panels_Data = SqliteDB.get_panel_group_data_api(id)
+    if(Panels_Data is not None):
+        Id_PanelGroup = Panels_Data["Id_PanelGroup"]
+        Person_id = Panels_Data["Person_id"]
+        Panels_amount = Panels_Data["Panels_amount"]
+        Panels_adress = Panels_Data["Panels_adress"]
+        SqliteDB.update_penels_group(Id_PanelGroup, Person_id, Panels_amount, Panels_adress, performance, voltage, power, id)
 
-@app.put("/weather/{id}", tags=["Example route"])
-async def weather(id: int, data: dict):
-    return {"message": "weather for {id}"}
 
 
 if __name__ == "__main__":
