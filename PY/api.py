f4d3c345bbe455c72371fbf2e75143cab901a3a7
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-07 22:53:07 +0200

    Update api.py

--- PY/api.py
+++ PY/api.py
@@ -1,14 +1,20 @@
+# import fasapi
 from fastapi import FastAPI
 
-# import db handlers
+# import db handler
 from db_handler import SqliteDB
 
 # import models
 from models import CheckModel, GroupDataModel
 
+# import requests for weather api
 import requests
 
+# import datetime for weather api
 from datetime import datetime
+
+#data about new solar panels group(if exist)
+new_group = {"id": None, "performance": None, "voltage": None, "power": None}
 
 app = FastAPI(title="SolarControl API", version="0.1.0", description="API for SolarControl project")
 
@@ -23,12 +29,15 @@ async def auth(auth_data: CheckModel):
     print(username)
     print(password)
 
+
+#get data from Arduino Uno WiFi Rev2
 @app.post("/group_data")
 async def add_group_data(group_data: GroupDataModel):
     id = group_data.id
     performance = group_data.performance
     voltage = group_data.voltage
     power = group_data.power
+
     Panels_Data = SqliteDB.get_panel_group_data_api(id)
     if(Panels_Data is not None):
         Id_PanelGroup = Panels_Data["Id_PanelGroup"]
@@ -37,10 +46,18 @@ async def add_group_data(group_data: GroupDataModel):
         Panels_adress = Panels_Data["Panels_adress"]
         SqliteDB.update_penels_group(Id_PanelGroup, Person_id, Panels_amount, Panels_adress, performance, voltage, power, id)
         get_weather_from_api(Panels_adress, Id_PanelGroup)
+    else:
+        #if in database is`t exist data about some panels group save this data to add their in future
+        global new_group
+        new_group['id'] =  id
+        new_group['performance'] =  performance
+        new_group['voltage'] =  voltage
+        new_group['power'] =  power
 
 
 
 
+#get data about weather in solar panels group adress and add to database
 def get_weather_from_api(location: str, Id_PanelGroup: int):
     api_key = 'e48976283ebb45a7ae1102438231003'
     date = datetime.today().strftime('%Y-%m-%d')
