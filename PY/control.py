51e5c1bb8085c0fcaa634f93a12c6ab94c9c9785
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-11 19:59:59 +0200

    Update control.py

--- PY/control.py
+++ PY/control.py
@@ -2,8 +2,13 @@ from PyQt5 import QtWidgets
 import Log_in
 import Sign_in
 import U_profile
+import Forgot_pass
+import Change_pass
+import Change_login
+from db_handler import SqliteDB
 
-class Control:
+
+class ControlWindow:
     def __init__(self):
         pass
     def show_login(self):
@@ -18,8 +23,25 @@ class Control:
         self.profile_window = U_profile.ProfileWindow()
         self.profile_window.show()
 
+    def show_forgot_pass(self):
+        self.forgot_pass = Forgot_pass.ForgotPassWindow()
+        self.forgot_pass.show()
+
+    def show_change_pass(self):
+        self.change_pass = Change_pass.ChangePassWindow()
+        self.change_pass.show()
+
+    def show_change_login(self):
+        self.change_login = Change_login.ChangeLoginWindow()
+        self.change_login.show()
+
+class Main:
+    def __init__(self):
+        pass
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    control = Control()
+    control = ControlWindow()
     control.show_login()
     app.exec_()
