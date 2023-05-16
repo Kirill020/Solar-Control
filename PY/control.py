9fd1cd6fd837ffe089c62d4f05bbae029b7c7d43
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-16 19:30:46 +0200

    Update control.py

--- PY/control.py
+++ PY/control.py
@@ -1,44 +1,67 @@
 from PyQt5 import QtWidgets
-import Log_in
-import Sign_in
-import U_profile
-import Forgot_pass
-import Change_pass
-import Change_login
+import Log_in_form
+import Sign_in_form
+import U_profile_form
+import Forgot_pass_form
+import Change_pass_form
+import Change_login_form
+import Change_name_form
+import api
 from db_handler import SqliteDB
 
 
 class ControlWindow:
+    
     def __init__(self):
-        pass
+        self.session_id = Log_in_form.session_id
+        
+        User_data, self.session_binary_avatar = SqliteDB.get_user_data(None, self.session_id)
+        if User_data is None:
+            self.session_login = None
+            self.session_name = None
+        else:
+            self.session_login = User_data["P_email_adress"]
+            self.session_name = User_data["P_name"]
+
     def show_login(self):
-        self.login_window = Log_in.LoginWindow()
+        self.login_window = Log_in_form.LoginWindow()
         self.login_window.show()
     
     def show_sign_in(self):
-        self.sign_in_window = Sign_in.RegistrationWindow()
+        self.sign_in_window = Sign_in_form.RegistrationWindow()
         self.sign_in_window.show()
 
     def show_profile(self):
-        self.profile_window = U_profile.ProfileWindow()
+        self.profile_window = U_profile_form.ProfileWindow()
         self.profile_window.show()
 
     def show_forgot_pass(self):
-        self.forgot_pass = Forgot_pass.ForgotPassWindow()
+        self.forgot_pass = Forgot_pass_form.ForgotPassWindow()
         self.forgot_pass.show()
 
     def show_change_pass(self):
-        self.change_pass = Change_pass.ChangePassWindow()
+        self.change_pass = Change_pass_form.ChangePassWindow()
         self.change_pass.show()
 
     def show_change_login(self):
-        self.change_login = Change_login.ChangeLoginWindow()
+        self.change_login = Change_login_form.ChangeLoginWindow()
         self.change_login.show()
 
-class Main:
-    def __init__(self):
-        pass
+    def show_change_name(self):
+        self.change_name = Change_name_form.ChangeNameWindow()
+        self.change_name.show()
 
+        
+class ControlAPI:
+    def __init__(self):
+        if api.new_group["id"] is not None:
+            self.new_goup = api.new_group
+        else:
+            self.new_goup = {"id": None, "performance": None, "voltage": None, "power": None}
+
+
+
+    
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
