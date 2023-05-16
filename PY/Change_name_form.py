ed3271ffeb7442f44c02719bbdfb6c8537b2869a
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-16 19:31:02 +0200

    Create Change_name_form.py

---
+++ PY/Change_name_form.py
@@ -0,0 +1,186 @@
+import sys
+import control
+from db_handler import SqliteDB
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class ChangeNameWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.controller = control.ControlWindow()
+        self.setWindowTitle("Change login")
+        self.setFixedSize(455,300)
+        self.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0.482198, y1:0.971, x2:0.497, y2:0.023, stop:0.0338983 rgba(46, 46, 46, 255), stop:1 rgba(168, 168, 168, 255));")
+
+
+
+        #Logo name(SOLAR CONTROL)
+        self.logo_name_lab = QtWidgets.QLabel(self)
+        self.logo_name_lab.setGeometry(QtCore.QRect(180, 60, 101, 31))
+        self.logo_name_lab.setToolTip("")
+        self.logo_name_lab.setAutoFillBackground(False)
+        self.logo_name_lab.setStyleSheet("border: 0px; background-color:rgba(0,0,0,0%);")
+        self.logo_name_lab.setObjectName("logo_name_lab")
+        self.logo_name_lab.setText("<html><head/><body><p align=\"center\">SOLAR CONTROL</p></body></html>")
+
+
+
+#Edit for old login(email)
+        self.new_name_edit = QtWidgets.QLineEdit(self)
+        self.new_name_edit.setGeometry(QtCore.QRect(105, 110, 241, 41))
+        self.new_name_edit.setStyleSheet("QLineEdit {\n"
+                        "color: #333;\n"
+                        "border: 2px solid #555;\n"
+                        "border-radius: 10px;\n"
+                        "border-style: inset;\n"
+                        "font: 9pt \"MS Shell Dlg 2\";\n"
+                        "min-width: 8em;\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #a6a6a6\n"
+                        ");\n"
+                        "padding: 5px;\n"
+                        "}\n"
+                        "\n"
+                        "QLineEdit:hover {\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+                        "radius: 1.35, stop: 0 #b4b4b4, stop: 1 #969696\n"
+                        ");\n"
+                        "}\n"
+                        "\n"
+                        "QLineEdit:pressed {\n"
+                        "border-style: inset;\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+                        ");\n"
+                        "}")
+        self.new_name_edit.setInputMask("")
+        self.new_name_edit.setToolTip("Your new name")
+        self.new_name_edit.setText("")
+        self.new_name_edit.setObjectName("new_name_edit")
+        self.new_name_edit.setWhatsThis("Edit for name")
+        self.new_name_edit.setPlaceholderText("New name")
+
+
+        #Logo image
+        self.logo_Login = QtWidgets.QLabel(self)
+        self.logo_Login.setGeometry(QtCore.QRect(75, 40, 91, 61))
+        self.logo_Login.setMinimumSize(QtCore.QSize(82, 45))
+        self.logo_Login.setStyleSheet("background-color: rgba(255, 255, 255, 0);\n"
+                        "image: url(:/newPrefix/images/backgrounds/PsLYIQ01.svg);")
+        self.logo_Login.setObjectName("logo_Login")
+        self.logo_Login.setText("<html><head/><body><p align=\"center\"><br/></p></body></html>")
+
+        
+
+
+#change name button 
+        self.ch_name_but = QtWidgets.QPushButton(self)
+        self.ch_name_but.setGeometry(QtCore.QRect(160, 175, 131, 41))
+        self.ch_name_but.setMinimumSize(QtCore.QSize(131, 41))
+        self.ch_name_but.setMaximumSize(QtCore.QSize(145, 41))
+        self.ch_name_but.setAutoFillBackground(False)
+        self.ch_name_but.setStyleSheet("QPushButton {\n"
+                        "color: #333;\n"
+                        "border: 2px solid #555;\n"
+                        "border-radius: 20px;\n"
+                        "border-style: outset;\n"
+                        "font: 12pt \"MS Shell Dlg 2\";\n"
+                        "max-width: 131px;\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+                        ");\n"
+                        "padding: 5px;\n"
+                        "}\n"
+                        "\n"
+                        "QPushButton:hover {\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+                        ");\n"
+                        "}\n"
+                        "\n"
+                        "QPushButton:pressed {\n"
+                        "border-style: inset;\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+                        ");\n"
+                        "}")
+        self.ch_name_but.setObjectName("ch_name_but")
+        self.ch_name_but.setText("Change")
+        self.ch_name_but.clicked.connect(self.change_name)
+
+
+
+
+
+#Back button 
+        self.back_but = QtWidgets.QPushButton(self)
+        self.back_but.setGeometry(QtCore.QRect(0, 0, 30, 30))
+        self.back_but.setAutoFillBackground(False)
+        self.back_but.setStyleSheet("QPushButton {\n"
+                        "color: #333;\n"
+                        "border: 2px solid #555;\n"
+                        "border-radius: 20px;\n"
+                        "border-style: outset;\n"
+                        "font: 12pt \"MS Shell Dlg 2\";\n"
+                        "max-width: 131px;\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+                        ");\n"
+                        "padding: 5px;\n"
+                        "}\n"
+                        "\n"
+                        "QPushButton:hover {\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+                        ");\n"
+                        "}\n"
+                        "\n"
+                        "QPushButton:pressed {\n"
+                        "border-style: inset;\n"
+                        "background: qradialgradient(\n"
+                        "cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+                        "radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+                        ");\n"
+                        "}")
+        self.back_but.setObjectName("back_but")
+        self.back_but.setText("<")
+        self.back_but.clicked.connect(self.back_to_profile)
+
+
+
+#change password and add data to database
+    def change_name(self):
+        if not self.new_name_edit.text():
+            QtWidgets.QMessageBox.warning(self, "Warning", "Fill every fields please!")
+        else:
+            
+            new_name = str(self.new_name_edit.text())
+            if SqliteDB.update_user_data(self.controller.session_id, new_name, None, None, None, None):
+                QtWidgets.QMessageBox.information(self, "Succesful!", "Your name has been changed!")
+                self.back_to_profile()
+            else:
+                QtWidgets.QMessageBox.warning(self, "Error!", "Something went wrong!")        
+                self.back_to_profile()
+
+#close window
+    def back_to_profile(self):
+        self.new_name_edit.setText("")
+        
+        self.controller.show_profile()
+        self.close()
+
+import Backgrounds
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    new_pass_window = ChangeNameWindow()
+    new_pass_window.show()
+    sys.exit(app.exec_())
