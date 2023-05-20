e1d4eda026777707cabcc74429de38a1d22005c8
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-05-20 19:51:03 +0200

    Update Sign_in_form.py

--- PY/Sign_in_form.py
+++ PY/Sign_in_form.py
@@ -302,6 +302,19 @@ class RegistrationWindow(QMainWindow):
         self.password_edit.setWhatsThis("Edit for login")
         self.password_edit.setPlaceholderText("Password")
         self.password_edit.setReadOnly(True)
+        self.password_edit.setEchoMode(QtWidgets.QLineEdit.Password)
+
+        self.show_password_checkbox = QtWidgets.QCheckBox(self)
+        self.show_password_checkbox.setText("Show")
+        self.show_password_checkbox.setGeometry(400,327,50,30)
+        self.show_password_checkbox.setStyleSheet("background-color: rgba(255, 255, 255, 0);\n")
+        self.show_password_checkbox.clicked.connect(self.show_password)
+
+    def show_password(self):
+        if self.show_password_checkbox.isChecked():
+            self.password_edit.setEchoMode(QtWidgets.QLineEdit.Normal)
+        else:
+            self.password_edit.setEchoMode(QtWidgets.QLineEdit.Password)
 
 
 
