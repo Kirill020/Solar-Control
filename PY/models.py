c67eb9a5a312966ec44703d0d6b72f88d051d045
Kyrylo Tanasiichuk <tanasiychukkirill@gmail.com>
2023-06-21 11:01:19 +0200

    Update models.py

--- PY/models.py
+++ PY/models.py
@@ -2,12 +2,23 @@ from pydantic import BaseModel
 from typing import Optional
 
 class LoginModel(BaseModel):
-    username: str
+    login: str
     password: str
 
 
 class AuthModel(BaseModel):
     token: str
+
+class VerifyEmailModel(BaseModel):
+    login: str
+    form_name: str
+
+class RegModel(BaseModel):
+    login: str
+    password: str
+    username: str
+    adress: str
+    code: int
 
 class GroupDataModel(BaseModel):
     id: int
